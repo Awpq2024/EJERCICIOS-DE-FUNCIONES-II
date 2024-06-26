b420c386a6b08f940823b0b80f138144de45426d
Awpq2024 <awpariq@unjbg.edu.pe>
2024-06-26 14:19:13 -0500

    Se agrega el quinto ejercicio, Juego de Piedra, Papel y Tijera, ejecuta el juego hasta que uno gane 3 veces la ronda y muestra al ganador

---
+++ Juego de Piedra, Papel y Tijera.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+string jugada_computadora(){
+	int numero=rand()%3;
+	
+	if (numero==0){
+		return "piedra";
+	}else if(numero==1){
+		return "papel";
+	}else{
+		return "tijera";
+	}
+}
+
+int main (){
+	int puntos_jugador=0, puntos_computadora=0;
+	string jugador,computadora;
+	
+	do{
+		cout<<"Ingrese su jugada, (piedra, papel o tijera): ";
+		cin>>jugador;
+		while (jugador != "piedra" && jugador != "papel" && jugador != "tijera") {
+            cout<<"Jugada invalida, por favor ingrese solo piedra, papel o tijera: ";
+            cin>>jugador;
+        }
+        
+		computadora=jugada_computadora();
+		cout<<"La computadora escogio: "<<computadora<<endl<<endl;
+        
+		if (jugador == computadora){
+			cout<<"Nadie gana esta ronda"<<endl;
+		}else if (jugador == "piedra" && computadora == "tijera"){
+			cout<<"El jugador gana esta ronda"<<endl;
+			puntos_jugador++;
+		}else if (jugador == "papel" && computadora == "piedra"){
+			cout<<"El jugador gana esta ronda"<<endl;
+			puntos_jugador++;	
+		}else if (jugador == "tijera" && computadora == "papel"){
+			cout<<"El jugador gana esta ronda"<<endl;	
+			puntos_jugador++;
+    	}else{
+    		cout<<"La computadora gana esta ronda"<<endl<<endl;
+    		puntos_computadora++;
+		}
+		cout<<"Jugador: "<<puntos_jugador<<" - Computadora : "<<puntos_computadora<<endl<<endl;
+		
+	}while (puntos_jugador < 3 && puntos_computadora < 3);
+	
+	if (puntos_jugador == 3){
+		cout<<"El ganador es el jugador";
+	}else{
+		cout<<"El ganador es la computadora";
+	}
+	
+	
+	return 0;
+}
